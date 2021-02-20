cbe5621e146369259ca21feea541dc42f8386352
maxyee <eyaminkhan00@gmail.com>
2021-02-21 03:18:51 +0600

    initial commit for the project

---
+++ src/test/java/com/example/carcollection/CarCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carcollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
